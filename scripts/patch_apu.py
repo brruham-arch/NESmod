ba2547063cf55a7ff460f0d235add4aef54b02a1
brruham <brruham@gmail.com>
2026-06-14 23:38:51 +0700

    fix: FrameCounter initializer list explicit vector type

--- scripts/patch_apu.py
+++ scripts/patch_apu.py
@@ -5,6 +5,19 @@ for fn in os.listdir(dst):
     if fn.endswith('.cpp'):
         path = os.path.join(dst, fn)
         txt = open(path).read()
-        txt = re.sub(r'#include\s*<SFML/[^>]+>', '', txt)
+        # Hapus SFML includes
+        txt = re.sub(r'#include\s*<SFML/[^>]+>\n?', '', txt)
         open(path, 'w').write(txt)
-        print(f'Patched: {fn}')
+        print(f'Patched SFML: {fn}')
+
+# Fix APU.cpp FrameCounter initializer list issue
+# APU.cpp:288 - FrameCounter({...}, irq) -> FrameCounter(std::vector<...>{...}, irq)
+apu = os.path.join(dst, 'APU.cpp')
+txt = open(apu).read()
+# Wrap initializer list with explicit vector type
+txt = txt.replace(
+    'return FrameCounter(\n        {',
+    'return FrameCounter(\n        std::vector<std::reference_wrapper<FrameClockable>>{'
+)
+open(apu, 'w').write(txt)
+print('Fixed FrameCounter initializer')
